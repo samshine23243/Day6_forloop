72e0ef9bebcc617af22085034bb1af662eed7c53
samshine <sameershinde23243@gmail.com>
2021-02-15 14:29:39 +0530

    example4 code added

--- for.sh
+++ for.sh
@@ -1,10 +1,26 @@
-read -p "Enter N:" N
-for ((i=1;i<=N;i++))
+read -p "Enter Number : " N
+for ((j=1;j<=N;j++))
 do
-   temp=1
-   for ((j=1;j<=i;j++))
-   do
-      temp=$(($temp*2))
-   done
-   echo $temp
+   if [ $j -eq "1" ] || [ $j -eq "2" ]
+   then
+       echo "Prime Number" $j
+   else
+      for ((i=2;i<j;i++))
+      do
+         temp=$(($j%$i))
+         if [ $temp -eq "0" ]
+         then
+            flag=1
+            break
+         else
+            flag=0
+         fi
+      done
+      if [ $flag -eq "1" ]
+      then
+         echo "Not Prime Number" $j
+      else
+         echo "Prime Number" $j
+      fi
+   fi
 done
